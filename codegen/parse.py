5201220685263fd87724e8964befea5bceb403b5
utensil <utensilcandel@gmail.com>
2023-09-26 22:47:26 +0800

    Initial attempt on mapping types
    1. Parse and register the class
    2. Parse method basics
    3. Start mapping param and return types
    
    TODO:
    
    1. Code clean up
    2. Better default to_lean, implement from_lean
    3. Handle *, &, const, void etc.
    4. Handle classes in param/return types
    5. Handle nested namespace
    6. Figure out how to configure exclude/include of methods/classes and the general pattern
    7. Test parse&generate

--- codegen/parse.py
+++ codegen/parse.py
@@ -11,15 +11,15 @@ from typing import (Any, Dict, Generator, Iterator, List, Optional, Sequence,
 
 import yaml
 from clang.cindex import (AccessSpecifier, Config, Cursor, CursorKind,
-                          FileInclusion, Index, TranslationUnit, TypeKind,
+                          FileInclusion, Index, TranslationUnit, TypeKind, Type as CppType,
                           conf)
 
 
-def get_cxx_include_paths():
+def get_cpp_include_paths():
     # run `clang -v -E -x c++ - -v < /dev/null 2>&1` to get the include paths
     result = subprocess.run(['clang', '-v', '-E', '-x', 'c++', '-', '-v'], input=b'', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = result.stderr.decode(sys.getdefaultencoding())
-    search_list = output.split('#include <...> search starts here:\n')[1].split('End of search list.')[0]
+    search_list = output.split('#include <...> search starts here:\n')[-1].split('End of search list.')[0]
     include_paths = []
     for line in search_list.split('\n'):
         line = line.strip()
@@ -27,8 +27,8 @@ def get_cxx_include_paths():
             include_paths.append(line)
     return include_paths
 
-cxx_include_paths = get_cxx_include_paths()
-print(f'C++ include paths: {cxx_include_paths}')
+cpp_include_paths = get_cpp_include_paths()
+print(f'C++ include paths: {cpp_include_paths}')
 
 index: Index = Index.create()
 
@@ -43,7 +43,7 @@ ginac_header: Path = dir_build / ginac / 'symbol.h'
 index_args: List[str] = ['-xc++', '-std=c++11']
 
 # Add C++ include paths to the index or libclang fail to recognize std::string etc.
-index_args.extend(['-I' + include_path for include_path in cxx_include_paths])
+index_args.extend(['-I' + include_path for include_path in cpp_include_paths])
 
 # print(index_args)
 
@@ -51,9 +51,21 @@ print(f'Parsing {ginac_header}...')
 ginac_ast: TranslationUnit = index.parse(ginac_header, args=index_args)
 
 @dataclass
+class ParamType:
+    lean: str
+    cpp: str
+    from_lean: str
+
+@dataclass
+class ReturnType:
+    lean: str
+    cpp: str
+    to_lean: str
+
+@dataclass
 class MethodParam:
     name: str
-    type: Dict[str, str]
+    type: ParamType
 
 @dataclass
 class Method:
@@ -61,10 +73,10 @@ class Method:
     lean: str
     cpp: str
     params: List[MethodParam]
-    return_type: Dict[str, str]
+    return_type: ReturnType
 
 @dataclass
-class Type:
+class ClassType:
     lean: str
     cpp: str
     methods: List[Method]
@@ -73,7 +85,7 @@ class Type:
 class ClassInterface:
     namespace: str
     deps: List[Dict[str, str]]
-    type: Type
+    type: ClassType
 
     def to_dict(self: ClassInterface):
         data_dict = {
@@ -92,19 +104,37 @@ class ClassInterface:
                 'lean': method.lean,
                 'cpp': method.cpp,
                 'params': [],
-                'return_type': method.return_type
+                'return_type': {
+                    'lean': method.return_type.lean,
+                    'cpp': method.return_type.cpp,
+                    'to_lean': method.return_type.to_lean
+                }
             }
 
             for param in method.params:
                 param_dict = {
                     'name': param.name,
-                    'type': param.type
+                    'type': {
+                        'lean': param.type.lean,
+                        'cpp': param.type.cpp,
+                        'from_lean': param.type.from_lean
+                    }
                 }
                 method_dict['params'].append(param_dict)
 
             data_dict['type']['methods'].append(method_dict)
 
         return data_dict
+    
+def map_primitive_type(s, cpp_type: CppType) -> str:
+    return f'LEAN_PRIMITIVE_{cpp_type.spelling}'
+    
+def to_lean_type_name(s, cpp_type: CppType):
+    if is_primitive_type(cpp_type.kind):
+        return map_primitive_type(s, cpp_type)
+    if '::' in s:
+        return to_lean_type_name(s.split('::')[-1], cpp_type)
+    return ''.join(word.capitalize() for word in s.split('_'))
 
 class EntityCollector:
     recursive: bool = True
@@ -114,10 +144,10 @@ class EntityCollector:
     def __init__(self, recursive):
         self.recursive = recursive
 
-    def in_cxx_include_paths(self, file_name: str) -> bool:
+    def in_cpp_include_paths(self, file_name: str) -> bool:
         # TODO: the whole parsing should be embeded in this class
-        # FIXME: cxx_include_paths refering to global variable
-        for include_path in cxx_include_paths:
+        # FIXME: cpp_include_paths refering to global variable
+        for include_path in cpp_include_paths:
             if include_path in file_name:
                 return True
         return False
@@ -125,7 +155,7 @@ class EntityCollector:
     def should_skip(self, file_name: str) -> bool:
         # print(f'Checking if {file_name} should be skipped...')
 
-        return file_name in self.visited_headers or self.in_cxx_include_paths(file_name)
+        return file_name in self.visited_headers or self.in_cpp_include_paths(file_name)
     
     def visit_file(self, included_filename : str):
         # print(f'Visiting {included_filename}...')
@@ -144,6 +174,22 @@ class EntityCollector:
         
         for cursor in ast.cursor.walk_preorder():
             yield cursor
+
+    def find_or_create_class_interface(self, type_name_cpp: str, cpp_type: CppType) -> ClassInterface:
+        # Caution! It's registered in its lean name
+        type_name_lean = to_lean_type_name(type_name_cpp, cpp_type)
+
+        if type_name_lean in self.data:
+            return self.data[type_name_lean]
+        else:
+            class_interface = ClassInterface(type_name_cpp, [],
+                                             ClassType(type_name_lean, type_name_cpp, []))
+            self.data[type_name_lean] = class_interface
+            return class_interface
+
+    def find_or_create_type(self, type_name_cpp: str, cpp_type: CppType) -> ClassType:
+        class_interface = self.find_or_create_class_interface(type_name_cpp, cpp_type)
+        return class_interface.type
 
 concerned_cursor_kinds : List[CursorKind] = [
     CursorKind.STRUCT_DECL,
@@ -181,6 +227,95 @@ def check_arg_valid(cursor: Cursor):
             return
         # TODO: gather all the warnings and print them at the end, then give hints on how to fix them
         print(f'WARN Invalid argument type detected: {cursor.type.spelling} != {token_spellings} at {cursor.location.file.name}:{cursor.location.line}:{cursor.location.column}', file=sys.stderr)
+# TODO: add more primitive types
+def is_primitive_type(typekind: TypeKind):
+    return typekind in [
+        TypeKind.BOOL,
+        # TypeKind.CHAR_U,
+        # TypeKind.UCHAR,
+        # TypeKind.CHAR16,
+        # TypeKind.CHAR32,
+        TypeKind.USHORT,
+        TypeKind.UINT,
+        TypeKind.ULONG,
+        TypeKind.ULONGLONG, 
+        TypeKind.UINT128,
+        # TypeKind.CHAR_S,
+        # TypeKind.SCHAR,
+        # TypeKind.WCHAR,
+        TypeKind.SHORT,
+        TypeKind.INT,
+        TypeKind.LONG,
+        TypeKind.LONGLONG,
+        TypeKind.INT128,
+        TypeKind.FLOAT,
+        TypeKind.DOUBLE,
+        TypeKind.LONGDOUBLE
+    ]
+
+# FIXME maybe all these primitive types are not meant to mapped to Lean built-in types
+MAP_PRIMITIVE_TYPE_TO_LEAN_TYPE: Dict[TypeKind, str] = {
+    TypeKind.BOOL: 'Bool',
+    # TypeKind.CHAR_U: 'UInt8',
+    # TypeKind.UCHAR: 'UInt8',
+    # TypeKind.CHAR16: 'UInt16',
+    # TypeKind.CHAR32: 'UInt32',
+    TypeKind.USHORT: 'UInt16',
+    TypeKind.UINT: 'UInt32',
+    TypeKind.ULONG: 'UInt64',
+    TypeKind.ULONGLONG: 'UInt64', 
+    TypeKind.UINT128: 'UInt128',
+    # TypeKind.CHAR_S: 'Int8',
+    # TypeKind.SCHAR: 'Int8',
+    # TypeKind.WCHAR: 'UInt32',
+    TypeKind.SHORT: 'Int16',
+    TypeKind.INT: 'Int32',
+    TypeKind.LONG: 'Int64',
+    TypeKind.LONGLONG: 'Int64',
+    TypeKind.INT128: 'Int128',
+    TypeKind.FLOAT: 'Float',
+    TypeKind.DOUBLE: 'Float',
+    TypeKind.LONGDOUBLE: 'Float'
+}
+
+def map_primitive_type(type_name: str, cpp_type: CppType):
+    if cpp_type.kind in MAP_PRIMITIVE_TYPE_TO_LEAN_TYPE:
+        return MAP_PRIMITIVE_TYPE_TO_LEAN_TYPE[cpp_type.kind]
+    else:
+        return f'LEAN_PRIMITIVE_{type_name}'
+
+def to_lean_method_name(method_name_cpp: str, cursor: Cursor) -> str:
+    if cursor.kind == CursorKind.CONSTRUCTOR:
+        return 'mk'
+    # TODO: should keep or change getters/setters?
+    else:
+        return method_name_cpp
+    
+def from_lean_function(param_type_cpp: str, cursor: Cursor) -> str:
+    if param_type_cpp in ['const std::string &', 'const char *']:
+        return 'lean_string_cstr(%s)'
+    else:
+        print(f'WARN Unknown from_lean_function for {param_type_cpp} at {cursor.location.file.name}:{cursor.location.line}:{cursor.location.column}', file=sys.stderr)
+        return 'UNKOWN_FROM_LEAN_FUNCTION'
+    
+def to_lean_function(param_type_cpp: str, cpp_type: CppType) -> str:
+    if param_type_cpp in ['const std::string &']:
+        return 'lean_mk_string(%s.c_str())'
+    elif param_type_cpp in ['const char *']:
+        return 'lean_mk_string(%s)'
+    else:
+        # print(f'WARN Unknown to_lean_function for {param_type_cpp} at {cursor.location.file.name}:{cursor.location.line}:{cursor.location.column}', file=sys.stderr)
+        print(f'INFO Default to_lean_function to as is for {param_type_cpp}') # at {cursor.location.file.name}:{cursor.location.line}:{cursor.location.column}')
+        return '%s'
+    
+def cursor_kind_spelling(kind: CursorKind) -> str:
+    if kind == CursorKind.CONSTRUCTOR:
+        return 'CONSTRUCTOR'
+    elif kind == CursorKind.CXX_METHOD:
+        return 'CXX_METHOD'
+    else:
+        print(f'WARN Unknown cursor_kind_spelling for {kind}', file=sys.stderr)
+        return 'UNKNOWN_CURSOR_KIND'
 
 for cursor in collector.walk(ginac_ast):
     if cursor.kind in concerned_cursor_kinds:
@@ -198,6 +333,47 @@ for cursor in collector.walk(ginac_ast):
                 print('\tDOC: ', cursor.brief_comment) # raw_comment
 
             if cursor.kind in [CursorKind.CONSTRUCTOR, CursorKind.CXX_METHOD]:
+                class_type_name = cursor.lexical_parent.spelling
+                class_cursor = cursor.lexical_parent
+                class_type = collector.find_or_create_type(class_type_name, cursor.lexical_parent.type)
+
+                return_type= ReturnType(
+                        cpp=class_type_name,
+                        lean=to_lean_type_name(class_type_name, class_cursor.type),
+                        to_lean=to_lean_function(class_type_name, class_cursor.type)
+                    ) if cursor.kind == CursorKind.CONSTRUCTOR else ReturnType(
+                        cpp=cursor.result_type.spelling,
+                        lean=to_lean_type_name(cursor.result_type.spelling, cursor.result_type),
+                        to_lean=to_lean_function(cursor.result_type.spelling, cursor.result_type)
+                    )
+
+                method = Method(
+                    kind=cursor_kind_spelling(cursor.kind),
+                    cpp=cursor.spelling,
+                    lean=to_lean_method_name(cursor.spelling, cursor),
+                    params=[],
+                    return_type=return_type
+                )
+
+                def to_lean_param_name(arg: Cursor) -> str:
+                    if arg.type.is_pod() and is_primitive_type(arg.type.kind):
+                        return map_primitive_type(arg.type.spelling, arg.type)
+                    elif arg.type.spelling == 'const std::string &':
+                        return '@&String'
+                    else:
+                        print(f'WARN Unknown to_lean_param_name for {arg.type.spelling} at {arg.location.file.name}:{arg.location.line}:{arg.location.column}', file=sys.stderr)
+                        return 'UNKOWN_LEAN_PARAM_NAME'
+
+                method.params = [MethodParam(
+                    name=arg.spelling,
+                    type=ParamType(
+                        cpp=arg.type.spelling,
+                        lean=to_lean_param_name(arg),
+                        from_lean=from_lean_function(arg.type.spelling, arg)
+                    )
+                ) for arg in cursor.get_arguments()]
+                class_type.methods.append(method)
+
                 for arg in cursor.get_arguments():
                     check_arg_valid(arg)
                     referenced = arg.referenced
@@ -230,8 +406,8 @@ for cursor in collector.walk(ginac_ast):
                     )
 
 for class_name, class_interface in collector.data.items():
-    if class_name == 'symbol':
-        class_interface['namespace'] = 'Ginac'
+    if class_name == 'Symbol':
+        class_interface.namespace = 'Ginac'
         with open(dir_data / f'{class_name}.yml', 'w') as file:
             yaml.dump(class_interface.to_dict(), file)
 
